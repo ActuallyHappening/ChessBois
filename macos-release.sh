37b0487e8e7db5ffc7593d8cd76c1f3b2c6adbda
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-06-23 14:25:45 +1000

    done: first release

---
+++ macos-release.sh
@@ -0,0 +1,11 @@
+cargo build --release --target x86_64-apple-darwin
+
+# Set ENV_BINARY to bevy_solver
+ENV_BINARY=bevy_solver
+
+mkdir -p $ENV_BINARY.app/Contents/MacOS
+cp target/x86_64-apple-darwin/release/$ENV_BINARY $ENV_BINARY.app/Contents/MacOS/
+cp -r assets $ENV_BINARY.app/Contents/MacOS/
+hdiutil create -fs HFS+ -volname "$ENV_BINARY" -srcfolder $ENV_BINARY.app $ENV_BINARY.dmg
+
+rm -rf $ENV_BINARY.app
