712395a44323744124845b2875ec53ea76851d06
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-07-07 16:40:36 +1000

    fix: styles on web are better

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use bevy::prelude::*;
-use bevy_solver::*;
 use bevy_solver::ChessPoint;
+use bevy_solver::*;
 
 fn main() {
 	App::new()
 		.register_type::<ChessPoint>()
-		.add_plugins(DefaultPlugins)
+		.add_plugins(
+			DefaultPlugins
+				.set(WindowPlugin {
+					primary_window: Some(Window {
+						fit_canvas_to_parent: true,
+						prevent_default_event_handling: false,
+						canvas: Some("#canvas".to_string()),
+						..default()
+					}),
+					..default()
+				})
+				.build(),
+		)
 		.add_plugin(bevy_editor_pls::prelude::EditorPlugin::default())
 		.add_plugin(ChessSolverPlugin)
 		.run();
