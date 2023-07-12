22b1cfc65283bbd1f547b557ffb7edcad0d8301d
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-07-12 20:08:06 +1000

    add: clipboard, check functional?

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,11 +1,12 @@
-#[cfg(not(target_os = "wasm32"))]
+#[cfg(not(target_arch = "wasm32"))]
 pub fn get_from_clipboard() -> String {
 	let mut clipboard = arboard::Clipboard::new().expect("Couldn't create clipboard instance");
 	clipboard.get_text().expect("Couldn't get text from clipboard")
 }
 
-#[cfg(target_os = "wasm32")]
+#[cfg(target_arch = "wasm32")]
 pub fn get_from_clipboard() -> String {
-	let clipboard = web_sys::Clipboard::new();
-	clipboard.read_text()
+	let clipboard = web_sys::window().expect("No window?").navigator().clipboard().expect("No clipboard?");
+	let res = futures::executor::block_on(wasm_bindgen_futures::JsFuture::from(clipboard.read_text())).expect("Future didn't execute?");
+	res.as_string().unwrap()
 }
