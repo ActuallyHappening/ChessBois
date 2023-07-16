0801c01585e564f501be69dfce805547be770678
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-07-16 17:36:42 +1000

    doc

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ set -exu
 ./windows-release.sh &
 ./web-release &
 
-# scp -r "ah@ahubuntu:/home/ah/ChessBois/releases/Caleb's MSRCQ11 v0.1.9" .
+# scp -r "ah@ahubuntu:/home/ah/ChessBois/releases/*" .
