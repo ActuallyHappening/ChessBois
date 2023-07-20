7c89490f5610a7601d7dfae0ddbc1f37b9040071
ActuallyHappening (Ubuntu) <105958073+ActuallyHappening@users.noreply.github.com>
2023-07-20 17:32:49 +1000

    add: opt for web

--- web-release.sh
+++ web-release.sh
@@ -1,6 +1,6 @@
 set -exu
 
-trunk build --release
+CARGO_PROFILE_RELEASE_OPT_LEVEL="s" trunk build --release
 
 VERSION=$(cargo get version --pretty)
 cp -r dist "releases/Caleb's MSRCQ11 ${VERSION}"
