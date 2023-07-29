1cd7bd80047bbd18c3190a1b4897dbd1d03a9875
ActuallyHappening (Ubuntu) <105958073+ActuallyHappening@users.noreply.github.com>
2023-07-29 20:13:50 +1000

    add: save old

--- src/board/manual/save.rs
+++ src/board/manual/save.rs
@@ -1,17 +1,16 @@
+use anyhow::Context;
 use bevy_egui::egui::Ui;
 use derive_more::Constructor;
 use serde::{Deserialize, Serialize};
-use anyhow::Context;
 
 use crate::{
 	board::{coloured_moves::ColouredMoves, SharedState},
-	solver::{pieces::StandardPieces, BoardOptions},
+	solver::BoardOptions,
 };
 
 #[derive(Serialize, Deserialize, Constructor)]
 pub struct SavedState {
 	pub moves: ColouredMoves,
-	pub piece: StandardPieces,
 	pub board_options: BoardOptions,
 }
 
@@ -20,7 +19,6 @@ impl TryFrom<SharedState> for SavedState {
 	fn try_from(value: SharedState) -> Result<Self, Self::Error> {
 		Ok(Self {
 			moves: value.moves.ok_or(())?,
-			piece: value.piece,
 			board_options: value.board_options,
 		})
 	}
@@ -28,24 +26,128 @@ impl TryFrom<SharedState> for SavedState {
 
 impl SavedState {
 	pub fn to_json(&self) -> String {
-		use jsonm::packer::*;
-		let mut packer = Packer::new();
-		let options = PackOptions::new();
-		
-		let packed = packer.pack(&self, &options).expect("Couldn't pack data");
-
-		serde_json::to_string(&packed).unwrap()
+		serde_json::to_string(&self).unwrap()
 	}
 
 	pub fn from_json(json: &str) -> Result<Self, anyhow::Error> {
-		use jsonm::unpacker::Unpacker;
+		match serde_json::from_str::<Self>(json) {
+			Ok(state) => Ok(state),
+			Err(new_err) => {
+				let old_err = old::try_depreciated_from_json(json);
+				if let Ok(state) = old_err {
+					return Ok(state);
+				}
+				Err(new_err).context("JSON dec`oding failed & depreciated failed")
+			}
+		}
+	}
+}
 
-		let json: serde_json::Value = serde_json::from_str(json).context("Cannot parse data as JSON")?;
+mod old {
+	use std::collections::HashSet;
 
-		let mut unpacker = Unpacker::new();
-		let unpacked: Self = unpacker.unpack(&json).context("Couldn't unpack data")?;
+	use anyhow::Context;
+	use serde::{Deserialize, Serialize};
 
-		Ok(unpacked)
+	use crate::solver::BoardOptions;
+
+	use super::SavedState;
+
+	/// Old didn't record targets or board size, so heuristics are added to the board options
+	pub fn try_depreciated_from_json(json: &str) -> Result<SavedState, anyhow::Error> {
+		let mut data: State = serde_json::from_str(json).context("JSON decoding failed")?;
+		let mut reached_points = HashSet::new();
+		for Move { from, to } in data.moves.moves.iter() {
+			reached_points.insert(from);
+			reached_points.insert(to);
+		}
+		let max_width = reached_points
+			.iter()
+			.map(|p| p.column)
+			.max()
+			.context("No moves")?;
+		let max_height = reached_points.iter().map(|p| p.row).max().unwrap();
+
+		let board_options = BoardOptions::new(max_width, max_height);
+		let moves = data
+			.moves.moves
+			.into_iter()
+			.map(|m| (m.into(), data.colours.pop().unwrap().into()))
+			.collect();
+
+		Ok(SavedState {
+			moves,
+			board_options,
+		})
+	}
+
+	#[test]
+	fn data1() {
+		let data = r#"{"start":{"column":1,"row":1},"moves":{"moves":[{"from":{"column":1,"row":1},"to":{"column":2,"row":3}},{"from":{"column":2,"row":3},"to":{"column":1,"row":5}},{"from":{"column":1,"row":5},"to":{"column":3,"row":6}},{"from":{"column":3,"row":6},"to":{"column":5,"row":5}},{"from":{"column":5,"row":5},"to":{"column":4,"row":3}},{"from":{"column":4,"row":3},"to":{"column":5,"row":1}},{"from":{"column":5,"row":1},"to":{"column":3,"row":2}},{"from":{"column":3,"row":2},"to":{"column":4,"row":4}},{"from":{"column":4,"row":4},"to":{"column":5,"row":6}},{"from":{"column":5,"row":6},"to":{"column":3,"row":5}},{"from":{"column":3,"row":5},"to":{"column":1,"row":6}},{"from":{"column":1,"row":6},"to":{"column":2,"row":4}},{"from":{"column":2,"row":4},"to":{"column":1,"row":2}},{"from":{"column":1,"row":2},"to":{"column":3,"row":1}},{"from":{"column":3,"row":1},"to":{"column":5,"row":2}},{"from":{"column":5,"row":2},"to":{"column":3,"row":3}},{"from":{"column":3,"row":3},"to":{"column":5,"row":4}},{"from":{"column":5,"row":4},"to":{"column":4,"row":2}},{"from":{"column":4,"row":2},"to":{"column":2,"row":1}},{"from":{"column":2,"row":1},"to":{"column":1,"row":3}},{"from":{"column":1,"row":3},"to":{"column":2,"row":5}},{"from":{"column":2,"row":5},"to":{"column":4,"row":6}},{"from":{"column":4,"row":6},"to":{"column":3,"row":4}},{"from":{"column":3,"row":4},"to":{"column":5,"row":3}},{"from":{"column":5,"row":3},"to":{"column":4,"row":1}},{"from":{"column":4,"row":1},"to":{"column":2,"row":2}},{"from":{"column":2,"row":2},"to":{"column":1,"row":4}},{"from":{"column":1,"row":4},"to":{"column":2,"row":6}},{"from":{"column":2,"row":6},"to":{"column":4,"row":5}},{"from":{"column":4,"row":5},"to":{"column":4,"row":5}}]},"colours":["Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green","Green"]}"#;
+		let state = try_depreciated_from_json(data).unwrap();
+	}
+
+	#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+	struct State {
+		moves: Moves,
+		colours: Vec<Colour>,
+	}
+
+
+	#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+	struct Moves {
+		moves: Vec<Move>,
+	}
+
+	#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+	struct ChessPoint {
+		column: u16,
+		row: u16,
+	}
+
+	impl From<ChessPoint> for crate::solver::ChessPoint {
+		fn from(value: ChessPoint) -> Self {
+			Self {
+				column: value.column,
+				row: value.row,
+			}
+		}
+	}
+
+	#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+	struct Move {
+		from: ChessPoint,
+		to: ChessPoint,
+	}
+
+	impl From<Move> for crate::solver::Move {
+		fn from(value: Move) -> Self {
+			Self {
+				from: value.from.into(),
+				to: value.to.into(),
+			}
+		}
+	}
+
+	#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+	enum Colour {
+		Green,
+		Blue,
+		Red,
+		Orange,
+		Invisible,
+	}
+
+	impl From<Colour> for crate::board::VizColour {
+		fn from(value: Colour) -> Self {
+			match value {
+				Colour::Green => Self::Green,
+				Colour::Blue => Self::Blue,
+				Colour::Red => Self::Red,
+				Colour::Orange => Self::Orange,
+				Colour::Invisible => Self::Invisible,
+			}
+		}
 	}
 }
 
@@ -64,7 +166,6 @@ impl SharedState {
 			let json = crate::clipboard::get_from_clipboard();
 			let state = SavedState::from_json(&json).unwrap();
 			self.moves = Some(state.moves);
-			self.piece = state.piece;
 			self.board_options = state.board_options;
 		}
 	}
