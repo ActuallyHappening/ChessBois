fab7050402049ade7d13839df4599ff46cd77a30
ActuallyHappening (Ubuntu) <105958073+ActuallyHappening@users.noreply.github.com>
2023-08-11 16:25:14 +1000

    fix: web-deploy

--- web-deploy.sh
+++ web-deploy.sh
@@ -1,11 +1,9 @@
 set -exu
 
-
-
 CARGO_PROFILE_RELEASE_OPT_LEVEL="s" trunk build --release --no-default-features
 
 VERSION=$(cargo get version --pretty)
 readonly NAME="Caleb's MSRCQ11 ${VERSION}"
 cp -r dist "releases/${NAME}"
 
-rsync -r "./releases/${NAME}" "digitalocean:/root/ChessBois/dist" 
+rsync -r "./releases/${NAME}" "digitalocean:/root/ChessBois/releases/${NAME}"
