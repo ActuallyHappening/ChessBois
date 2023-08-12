702d67010e6cbc970e41475fc23f1702467320fa
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-08-12 13:47:29 +1000

    _

--- web-deploy.sh
+++ web-deploy.sh
@@ -3,7 +3,7 @@ set -exu
 CARGO_PROFILE_RELEASE_OPT_LEVEL="s" trunk build --release --no-default-features
 
 VERSION=$(cargo get version --pretty)
-readonly NAME="Caleb's MSRCQ11 ${VERSION}"
+readonly NAME="Calebs_MSRCQ11_${VERSION}"
 cp -r dist "releases/${NAME}"
 
-rsync -r "./releases/${NAME}" "digitalocean:/root/ChessBois/releases/${NAME}"
+rsync -r "./releases/${NAME}" "digitalocean:/root/ChessBois/releases/"
